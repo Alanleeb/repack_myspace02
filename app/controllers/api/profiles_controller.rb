5f92815ab3f76aed12164870121ef9a47bc900ae
Alan Bischoff <alanlbischoff@gmail.com>
2018-06-24 21:29:57 -0600

    added models and controllers

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,22 +1,41 @@
 class Api::ProfilesController < ApplicationController
+  before_action :set_profile, only: [:show, :update, :destroy]
 
   def index
-    render json: Profile.all
+    render json: Profile.all.order(created_at: :desc)
+  end
+
+  def show
+    render json: @profile
+  end
+
+  def create
+    app = Profile.create(profile_params)
+    if profile.save
+      render json: profile
+    else
+      render json: { errors: profile.errors.full_messages.join(',') }, status: 422
+    end
   end
 
   def update
+    if @profile.update(profile_params)
+      render json: @profile
+    else
+      render json: { errors: @profile.errors.full_messages.join(',') }, status: 422
+    end
   end
+
+  def destroy
+    @profile.destroy
+  end
+
+  private
+    def set_profile
+      @profile = Profile.find(params[:id])
+    end
+
+    def app_profile
+      params.require(:profile).permit(:user_name, :age, :contact, :avatar, :quote)
+    end
 end
-
-
-
-# class Api::CatsController < ApplicationController
-#   def index
-#     render json: User.random_cat(current_user.liked_cats)
-#   end
-
-#   def update
-#     current_user.liked_cats << params[:id].to_i
-#     current_user.save
-#   end
-# end
